606a616d098452ed83e58ca3b2ac39e892167ceb
Zhenyu001225 <vincent001225@gmail.com>
2023-09-28 10:34:34 -0400

    test2

---
+++ KVCache.py
@@ -0,0 +1,29 @@
+import torch
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+
+model = GPT2LMHeadModel.from_pretrained("/WORK/Test/gpt", torchscript=True).eval()
+
+# tokenizer
+tokenizer = GPT2Tokenizer.from_pretrained("/WORK/Test/gpt")
+in_text = "Lionel Messi is a"
+in_tokens = torch.tensor(tokenizer.encode(in_text))
+
+# inference
+token_eos = torch.tensor([198]) # line break symbol
+out_token = None
+kvcache = None
+out_text = in_text
+i = 0
+with torch.no_grad():
+    while out_token != token_eos:
+        logits, kvcache = model(in_tokens, past_key_values=kvcache) # 增加了一个 past_key_values 的参数
+        out_token = torch.argmax(logits[-1, :], dim=0, keepdim=True)
+        in_tokens = out_token # 输出 token 直接作为下一轮的输入，不再拼接
+        text = tokenizer.decode(in_tokens)
+        print(f'step {i} input: {text}', flush=True)
+        i += 1
+        out_text += text
+
+print(f' Input: {in_text}')
+print(f'Output: {out_text}')
